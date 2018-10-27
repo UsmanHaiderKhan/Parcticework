f8236cd54d692869fe8257f3a819d0dc45640a7e
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-27 17:40:38 +0500

    Update Added new Script

--- src/javascript.js
+++ src/javascript.js
@@ -4,6 +4,43 @@
 // console.log(popularNumber);
 
 
-var num = 1;
-var new_num = num++;
-console.log(new_num);
+// var num = 1;
+// var new_num = num++;
+// console.log(new_num);
+
+
+// var total_cost=((2+1)+4*6)*4+1;
+// console.log(total_cost);
+
+// var name;
+// alert("Hello "+window.prompt("Enter Your Name:"+name))
+// console.log(name);
+
+//Simple Alert Form
+// var name="Usman Haider Khan";
+// var text="My Name is ";
+// var job="Software Enginner"
+
+// alert( text+" "+name+" "+job);
+
+//if Else statements
+
+// var x=prompt("Tell Me the Name of Your Favourite Ice Cream falviour: ");
+// if(x==="usman"){
+//   alert(x+""+ " Your choice is tooo good");
+// }
+// else if(x!=="usman"){
+//   alert(x+""+"Your Choice is too much bad");
+// }
+
+var weight = 70;
+var name = "usman";
+var height = 6;
+var lastname = "khan"
+var gender = "Male"
+
+if (weight < 70 || gender === "Male" && name === "usman" && lastname === "khan" && height === 6) {
+    alert("Usman haider khan properties");
+} else {
+    alert('Khan Khan khan');
+}
