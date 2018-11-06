fe359147541cc1e9a64b1b18e19b39cf96fb2356
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-11-06 17:13:26 +0500

    Update Added NEw ForLoop method or complete use full Example

--- src/javascript.js
+++ src/javascript.js
@@ -405,3 +405,27 @@
 // console.log(Account.name);
 // Account.deposit(3000);
 // Account.withdraw(1000);
+
+//Built Function
+
+var person = {
+    name: "usman haider khan",
+    age: 21,
+    saim: function () {
+        console.log("Hello Mt Dear :" + this.name);
+
+    }
+}
+for (var fields in person) {
+    console.log(fields);
+}
+
+console.log('name' in person);
+
+
+
+// var fun = ["usman", "khan", "salman"];
+
+// for (var i = 0; i < fun.length; i++) {
+//     console.log(fun[i]);
+// }
