7e3cf01358f702106473d13e2af601798815c9fb
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-11-18 12:15:28 +0500

    Update Created Methods to display the text

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 var search_Button = document.querySelector('button');
 var searchCity = document.querySelector("#city");
+
+
+var loadText = document.querySelector("#load");
+var weatherBox = document.querySelector("#Weather");
+
+var weatherCity = weatherBox.firstElementChild;
+var weatherDescription = document.querySelector("#weatherDescription");
+var weatherTemprature = weatherBox.lastElementChild;
